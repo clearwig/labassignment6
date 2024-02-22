36ff8f8771294d20c4b01ee995638adbe36e07da
clearwig <unarboreal@gmail.com>
2024-02-22 12:19:16 -0500

    Commit

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,7 +1,36 @@
-#include <stdio.h>
+/*
+Trae Forde
+Feburary 22, 2024
+COP 3502C
+Professor Torosdagli
+*/
 
-int search(int numbers[], int low, int high, int value) 
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(int numbers[], int low, int high, int value)
 {
+	if (low <= high)
+	{
+		int mid = low + (high - low) / 2;
+
+		if (numbers[mid] == value)
+		{
+			// Element found at mid index
+			return mid;
+		}
+		else if (numbers[mid] < value)
+		{
+			// Search the right half of the array
+			return search(numbers, mid + 1, high, value);
+		}
+		else
+		{
+			// Search the left half of the array
+			return search(numbers, low, mid - 1, value);
+		}
+	}
+
 	return -1;
 }
 
@@ -9,9 +38,9 @@ void printArray(int numbers[], int sz)
 {
 	int i;
 	printf("Number array : ");
-	for (i = 0;i<sz;++i)
+	for (i = 0; i < sz; ++i)
 	{
-		printf("%d ",numbers[i]);
+		printf("%d ", numbers[i]);
 	}
 	printf("\n");
 }
@@ -19,24 +48,24 @@ void printArray(int numbers[], int sz)
 int main(void)
 {
 	int i, numInputs;
-	char* str;
+	char *str;
 	float average;
 	int value;
 	int index;
-	int* numArray = NULL;
+	int *numArray = NULL;
 	int countOfNums;
-	FILE* inFile = fopen("input.txt","r");
+	FILE *inFile = fopen("input.txt", "r");
 
 	fscanf(inFile, " %d\n", &numInputs);
-	
+
 	while (numInputs-- > 0)
 	{
 		fscanf(inFile, " %d\n", &countOfNums);
-		numArray = (int *) malloc(countOfNums * sizeof(int));
+		numArray = (int *)malloc(countOfNums * sizeof(int));
 		average = 0;
 		for (i = 0; i < countOfNums; i++)
 		{
-			fscanf(inFile," %d", &value);
+			fscanf(inFile, " %d", &value);
 			numArray[i] = value;
 			average += numArray[i];
 		}
@@ -46,7 +75,7 @@ int main(void)
 		index = search(numArray, 0, countOfNums - 1, value);
 		if (index >= 0)
 		{
-			printf("Item %d exists in the number array at index %d!\n",value, index);
+			printf("Item %d exists in the number array at index %d!\n", value, index);
 		}
 		else
 		{
